567ec3175929ba264c2e055e509a483988ef0b7a
snowiow <marcel.patzwahl@posteo.de>
2021-07-23 16:53:26 +0200

    Initial State

---
+++ node_modules/@aws-sdk/util-credentials/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+# [3.22.0](https://github.com/aws/aws-sdk-js-v3/compare/v3.21.0...v3.22.0) (2021-07-16)
+
+
+### Features
+
+* **credential-provider-sso:** support sso credential when resolving shared credential file ([#2583](https://github.com/aws/aws-sdk-js-v3/issues/2583)) ([9480e70](https://github.com/aws/aws-sdk-js-v3/commit/9480e70da4ac59d4d08f01702b4e62bf42397394))
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import * as core from '@actions/core';
+import {
+  CodePipelineClient,
+  StartPipelineExecutionCommand,
+  GetPipelineExecutionCommand,
+  PipelineExecutionStatus
+} from '@aws-sdk/client-codepipeline';
+
+const CLIENT = new CodePipelineClient({});
+
+function sleep(s: number) {
+  return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
+
+async function waitForPipeline(pipelineName: string, pipelineExecutionId: string): Promise<boolean> {
+  await sleep(10);
+  const command = new GetPipelineExecutionCommand({ pipelineName, pipelineExecutionId });
+  CLIENT
+    .send(command)
+    .then((data) => {
+      switch (data.pipelineExecution.status) {
+        case PipelineExecutionStatus.InProgress:
+          core.info(`Pipeline '${pipelineName}' in progress waiting for 10 more seconds.`)
+          return waitForPipeline(pipelineName, pipelineExecutionId);
+        case PipelineExecutionStatus.Failed:
+          core.info(`Pipeline '${pipelineName}' failed.`)
+          return false;
+        case PipelineExecutionStatus.Stopping || PipelineExecutionStatus.Stopped:
+          core.info(`Pipeline '${pipelineName}' stopped.`)
+          return false;
+        case PipelineExecutionStatus.Superseded:
+          core.info(`Pipeline '${pipelineName}' was superseded.`)
+          return false;
+        case PipelineExecutionStatus.Cancelled:
+          core.info(`Pipeline '${pipelineName}' was canceled. Trying to get new execution ID.`)
+          //TODO: To implement
+          return false;
+
+        case PipelineExecutionStatus.Succeeded:
+          core.info(`Pipeline '${pipelineName}' succeeded.`)
+          return true;
+      }
+    })
+    .catch((error) => {
+      throw new Error(`An error occured while getting the Status of pipeline '${pipelineName}' exucution: '${pipelineExecutionId}'.\n ${error}`);
+    });
+
+  return false;
+}
+
+export async function run() {
+  const pipelineName: string = core.getInput('pipeline-name', { required: true });
+  const wait: boolean = core.getBooleanInput('wait', { required: false });
+
+  const command = new StartPipelineExecutionCommand({ name: pipelineName });
+
+  var executionId = '';
+
+  CLIENT
+    .send(command)
+    .then((data) => {
+      executionId = data.pipelineExecutionId;
+    }).catch((error) => {
+      core.error(`An error occured while starting Codepipeline '${pipelineName}': '${error}'`);
+    });
+
+  if (wait) {
+    const executionResult = await waitForPipeline(pipelineName, executionId);
+    if (!executionResult) {
+      core.error(`Execution was unsucessful.`);
+    }
+  }
+}
