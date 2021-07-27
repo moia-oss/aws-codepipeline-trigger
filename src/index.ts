3cab5b3653ccfc2f77950ff9710c1b2a9aaf8720
snowiow <marcel.patzwahl@posteo.de>
2021-07-27 17:48:12 +0200

    Add GitHub Workflow
    And therefore fix existing linter and format issues

--- src/index.ts
+++ src/index.ts
@@ -3,55 +3,57 @@ import {
   CodePipelineClient,
   StartPipelineExecutionCommand,
   GetPipelineExecutionCommand,
-  PipelineExecutionStatus
+  PipelineExecutionStatus,
 } from '@aws-sdk/client-codepipeline';
 
 const CLIENT = new CodePipelineClient({});
 
-function sleep(s: number) {
-  return new Promise(resolve => setTimeout(resolve, s * 1000));
-}
+const sleep = (s: number) => new Promise((resolve) => setTimeout(resolve, s * 1000));
 
-async function waitForPipeline(pipelineName: string, pipelineExecutionId: string): Promise<boolean> {
+const waitForPipeline = async (pipelineName: string, pipelineExecutionId: string): Promise<boolean> => {
   await sleep(10);
   const command = new GetPipelineExecutionCommand({ pipelineName, pipelineExecutionId });
   try {
-    const data = await CLIENT.send(command)
+    const data = await CLIENT.send(command);
 
     if (data.pipelineExecution === undefined || !data.pipelineExecution.status) {
       core.error('No Status for Pipeline');
       return false;
     }
-    const status = data.pipelineExecution.status;
+    const { status } = data.pipelineExecution;
     switch (status) {
       case PipelineExecutionStatus.InProgress:
-        core.info(`Pipeline '${pipelineName}' in progress waiting for 10 more seconds.`)
-        return waitForPipeline(pipelineName, pipelineExecutionId);
+        core.info(`Pipeline '${pipelineName}' in progress waiting for 10 more seconds.`);
+        return await waitForPipeline(pipelineName, pipelineExecutionId);
       case PipelineExecutionStatus.Failed:
-        core.info(`Pipeline '${pipelineName}' failed.`)
+        core.info(`Pipeline '${pipelineName}' failed.`);
         return false;
       case PipelineExecutionStatus.Stopping || PipelineExecutionStatus.Stopped:
-        core.info(`Pipeline '${pipelineName}' stopped.`)
+        core.info(`Pipeline '${pipelineName}' stopped.`);
         return false;
       case PipelineExecutionStatus.Superseded:
-        core.info(`Pipeline '${pipelineName}' was superseded.`)
+        core.info(`Pipeline '${pipelineName}' was superseded.`);
         return false;
       case PipelineExecutionStatus.Cancelled:
-        core.info(`Pipeline '${pipelineName}' was canceled. Trying to get new execution ID.`)
-        //TODO: To implement
+        core.info(`Pipeline '${pipelineName}' was canceled. Trying to get new execution ID.`);
+        // TODO: To implement
         return false;
 
       case PipelineExecutionStatus.Succeeded:
-        core.info(`Pipeline '${pipelineName}' succeeded.`)
+        core.info(`Pipeline '${pipelineName}' succeeded.`);
         return true;
+      default:
+        throw new Error(`Unexpected status: ${status} given.`);
     }
-    return false;
   } catch (error) {
-    throw new Error(`An error occured while getting the status of pipeline '${pipelineName}' exucution: '${pipelineExecutionId}'.\n ${error}`);
+    core.error(
+      `An error occured while getting the status of pipeline '${pipelineName}' exucution: '${pipelineExecutionId}'.`,
+    );
+    throw error;
   }
-}
+};
 
-export async function run() {
+const run = async (): Promise<void> => {
   const pipelineName: string = core.getInput('pipeline', { required: true });
   const wait: boolean = core.getBooleanInput('wait', { required: false });
 
@@ -60,20 +62,18 @@ export async function run() {
   try {
     const data = await CLIENT.send(command);
     if (!data.pipelineExecutionId) {
-      core.error('No Execution ID');
       throw new Error('No Execution ID');
     }
     if (wait) {
       const executionResult = await waitForPipeline(pipelineName, data.pipelineExecutionId);
       if (!executionResult) {
-        core.error('Execution was unsucessful.');
-        throw new Error('Execution was unsucessful.')
+        throw new Error('Execution was unsucessful.');
       }
     }
   } catch (error) {
-    core.error(`An error occured while starting Codepipeline '${pipelineName}': '${error}'`);
-    throw new Error(`An error occured while starting Codepipeline '${pipelineName}': '${error}'`)
+    core.error(`An error occured while starting Codepipeline '${pipelineName}'`);
+    throw error;
   }
-}
+};
 
 run().catch(core.setFailed);
