88b0def08c00e71034d83dd9acca7100f49e78c3
cbrgm <chris@cbrgm.net>
2023-01-18 17:58:26 +0100

    chore(renovate): add/update renovate configuration and workflows

---
+++ .github/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  dryRun: null,
+  username: 'moia-renovate',
+  gitAuthor: 'MOIA Renovate Bot <268291+moia-renovate@users.noreply.github.enterprise.com>',
+  onboarding: false,
+  platform: 'github',
+  includeForks: false,
+  repositories: ['moia-dev/aws-codepipeline-trigger'],
+};
